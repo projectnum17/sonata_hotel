0dae448848a44fbad5213bc428923d26c6cfece9
projectnum17 <egor.chornyy@gmail.com>
2026-08-18 17:50:51 +0300

    Updates

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,26 @@
 'use strict';
 
-document.addEventListener('DOMContentLoaded', () => {});
+const initHeader = () => {
+    const header = document.querySelector('.js-header');
+    if (!header) return;
+
+    let lastScroll = 0;
+    let scrollWay = 200;
+
+    const handleScroll = () => {
+        const currentScroll = window.scrollY;
+        header.classList.toggle('is-colored', currentScroll > 50);
+
+        currentScroll > scrollWay && currentScroll > lastScroll
+            ? header.classList.add('is-transform')
+            : header.classList.remove('is-transform');
+        lastScroll = scrollWay;
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    handleScroll();
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+    initHeader();
+});
